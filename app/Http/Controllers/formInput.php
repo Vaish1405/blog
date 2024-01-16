98ec0c79ae8df445b04eb38310cd3832426447a8
Vaishnavi Sen <vaishnavisen@Vaishnavis-MacBook-Pro.local>
2024-01-16 13:29:21 -0800

    error validation added

--- app/Http/Controllers/formInput.php
+++ app/Http/Controllers/formInput.php
@@ -6,8 +6,21 @@ use Illuminate\Http\Request;
 class FormInput extends Controller {
     public function processForm(Request $request)
     {
-        $data = $request->only(['id', 'title', 'summary', 'imageUrl', 'author', 'creationDate', 'updateDate', 'tags', 'commentsCount']);
+        // $data = $request->only(['id', 'title', 'summary', 'imageUrl', 'author', 'creationDate', 'updateDate', 'tags', 'commentsCount']);
         
+        $validatedData = $request->validate([
+            'id' => 'required',
+            'title' => 'required',
+            'author' => 'required',
+            'commentsCount' => 'required'
+        ], [
+            'id.required' => 'Id is required',
+            'title.required' => 'Title is required',
+            'author.required' => 'Auther name is required',
+            'commentsCount.required' => 'Provide the number of comments'
+        ]);
+        
+        $data = $validatedData->only(['id', 'title', 'summary', 'imageUrl', 'author', 'creationDate', 'updateDate', 'tags', 'commentsCount']);
         $jsonData = json_encode($data);
 
         return response()->json($jsonData);
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -8,29 +8,46 @@
 <body>
     <form action="{{ route('processForm') }}" method="POST">
         @csrf
+
         <label for="id">Id:</label>
-        <input type="text" id="id" name="id" required><br>
+        <input type="text" id="id" name="id" value="{{ old('id') }}">
+        @error('id')
+            <div style="color: red;">{{ $message }}</div>
+        @enderror
+        <br>
 
         <label for="title">Title:</label>
-        <input type="text" id="title" name="title" required><br>
+        <input type="text" id="title" name="title" value="{{ old('title') }}">
+        @error('title')
+            <div style="color: red;">{{ $message }}</div>
+        @enderror
+        <br>
 
         <label for="summary">Summary:</label>
-        <input type="text" id="summary" name="summary" required><br>
+        <input type="text" id="summary" name="summary" value="{{ old('summary') }}" required><br>
 
         <label for="author">Author:</label>
-        <input type="text" id="author" name="author" required><br>
+        <input type="text" id="author" name="author" value="{{ old('author') }}">
+        @error('author')
+            <div style="color: red;">{{ $message }}</div>
+        @enderror
+        <br>
 
         <label for="creationDate">Date of Creation:</label>
-        <input type="date" id="creationDate" name="creationDate" required><br>
+        <input type="date" id="creationDate" name="creationDate" value="{{ old('creationDate') }}" required><br>
 
         <label for="updateDate">Date of Update:</label>
-        <input type="date" id="updateDate" name="updateDate" required><br>
+        <input type="date" id="updateDate" name="updateDate" value="{{ old('updateDate') }}" required><br>
 
         <label for="tags">Tags:</label>
-        <input type="text" id="tags" name="tags" required><br>
+        <input type="text" id="tags" name="tags" value="{{ old('tags') }}" required><br>
 
         <label for="commentsCount">No. of Comments:</label>
-        <input type="number" id="commentsCount" name="commentsCount" required><br>
+        <input type="text" id="commentsCount" name="commentsCount" value="{{ old('commentsCount') }}">
+        @error('commentsCount')
+            <div style="color: red;">{{ $message }}</div>
+        @enderror
+        <br>
 
         <button type="submit">Submit</button>
     </form>
